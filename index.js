3aefdf749e2905573677357800b45f773494c95a
github username <github email address>
2019-09-26 16:26:06 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide() {
+    return 2000/100
+}
+
+let square = x => x*x
+
+let add = (x,y) => x + y
+
+
+// let twoAdder = x => x + 2;
+// // is the same as
+// let twoAdder = (x) => x + 2;
